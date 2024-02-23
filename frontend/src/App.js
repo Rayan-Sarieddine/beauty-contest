6355a74646db8f13089f4543c13832d110a41e5f
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-02-23 13:30:06 +0200

    <frontend> implemented domain and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./styles/index.css";
+import Home from "./domain/pages/Home";
+import Login from "./domain/pages/Login";
+import SignUp from "./domain/pages/SignUp";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
